bb2376ad862ca0dfc9102155773315c9501c2282
Ivan Lobanov <lobanovworkya@yandex-team.ru>
2024-11-10 21:13:16 +0300

    vue-pizza: add common components

---
+++ frontend/src/common/components/AppSheet.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sheet">
+    <h2 class="title title--small sheet__title">{{ title }}</h2>
+    <div :class="`sheet__content ${extraContentClasses}`">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import {defineProps} from 'vue';
+
+  const props = defineProps({
+    title: String,
+    extraContentClasses: String,
+  });
+</script>
